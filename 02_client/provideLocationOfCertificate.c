d9cd8d2832fa2c23f29164feef28a1ece4e4cad1
Marc <h.marc@studenti.unipi.it>
2022-04-18 02:21:23 +0200

    Added comments, to keep a better overview and to take notes.

--- 02_client/provideLocationOfCertificate.c
+++ 02_client/provideLocationOfCertificate.c
@@ -4,20 +4,40 @@
 #include <unistd.h>
 
 int askCertClient() {
-    printf("Please provide the location Security Certificate in order to Login:\n");
     
+    /* Terminal asks for the File Location of the Certificate File.
+        User then provides it using the scanf
+        
+
+        to:do prevent overflows, e.g. when file location is > 300
+        */
+    printf("Please provide the location Security Certificate in order to Login:\n");
     char fileLocation[300];
     scanf("%s", fileLocation);
     
+    /*Opens file
+
+    to do: prevent overflows
+        */
+
     FILE* ptr;
     char str[500];
     ptr = fopen(fileLocation, "a+");
-
     if (NULL == ptr) {
         printf("file can't be opened \n");
     }
- 
+
+    /* reads the content of the file
+        todo: prevent overlows
+        */
+
     fgets(str, 501, ptr);
+
+    /* verifies that legth of str is 128. I do not know why I need to make != 130 then but ok. 
+
+        If the str is not 128 chars longs, a error message is given to the Client. The entire method then repeats by calling it again (I do not know if you should call this way, we need to figure out)
+
+        */
 
     if(strlen(str) != 130) {
         printf("This is the wrong file / File is corrupted.\nPlease try again. \n");
