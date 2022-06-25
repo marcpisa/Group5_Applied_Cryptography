3ff1cfb5818d15b467cf8809aa75b117764c6680
FRubinoC <france.rubino1@gmail.com>
2022-06-25 19:19:45 +0200

    jkdalkd

--- old_projects/Project Structure Implementation/client/intclient.h
+++ old_projects/Project Structure Implementation/client/intclient.h
@@ -1,8 +1,8 @@
 #include <utilclient.h>
 int createSocket();
 int listClient(char* username, struct sockaddr_in srv_addr);
-int renameClient(...);
-int deleteClient(...);
-int downloadClient(...);
-int uploadClient(...);
-int shareClient(...);
+int renameClient();
+int deleteClient();
+int downloadClient();
+int uploadClient();
+int shareClient();
