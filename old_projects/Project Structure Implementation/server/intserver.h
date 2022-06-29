32c0f1dcf37e1d764903e0387b53995ff18cf4c7
FRubinoC <france.rubino1@gmail.com>
2022-06-29 15:30:31 +0200

    Update intserver.h

--- old_projects/Project Structure Implementation/server/intserver.h
+++ old_projects/Project Structure Implementation/server/intserver.h
@@ -1,8 +1,8 @@
 #include "utilserver.h"
 int createSocket();
 int listServer(int sd, char* rec_mex);
-int renameServer(int sd, char *rec_mex);
-int deleteServer(int sd, char *rec_mex);
+int renameServer(int sd, char* rec_mex);
+int deleteServer(int sd, char* rec_mex);
 int downloadServer();
 int uploadServer();
 int shareServer();
