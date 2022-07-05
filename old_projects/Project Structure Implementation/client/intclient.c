bab86599cdec098e91a943af5ba2eb225538dd1b
FRubinoC <france.rubino1@gmail.com>
2022-07-05 18:50:19 +0200

    Update intclient.c

--- old_projects/Project Structure Implementation/client/intclient.c
+++ old_projects/Project Structure Implementation/client/intclient.c
@@ -271,7 +271,7 @@ int downloadClient(char* username, char* filename, struct sockaddr_in srv_addr)
         {
             bufferSupp3[j] = buffer[position+j];
         }
-        bufferSupp3[j] = ´\0´;
+        bufferSupp3[j] = '\0';
         
         
         // Now take the bufferSupp3 and append it to the file. When the loop is over we close the file and we got what we neededs
