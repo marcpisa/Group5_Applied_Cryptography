96c4d6671304414d6cf33d7c0b1bf21d0662537b
Marc <h.marc@studenti.unipi.it>
2022-09-09 23:08:24 +0200

    Staged some stuff that happend because of mixed versions

--- src/util.h
+++ src/util.h
@@ -32,11 +32,6 @@
 #define SERVER_PORT 25020
 #define LOCALHOST "127.0.0.1"
 #define MEX_TYPE_LEN 8
-<<<<<<< HEAD
-#define MAIN_FOLDER_SERVER "../../database/"  
-#define MAIN_FOLDER_CLIENT "../../download/"  
-#define INFO_FOLDER_SERVER "../../database/info/"
-=======
 #define MAIN_FOLDER_SERVER "../../database"  // When you test the software on your pc change this variable
 #define MAIN_FOLDER_CLIENT "../../download"  // When you test the software on your pc change this variable
 #define INFO_FOLDER_SERVER "../../database/info" // When you test the software on your pc change this variable
